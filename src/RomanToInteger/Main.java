dd32cff807257498b33962fdd3dcab282be5d281
Thejaswi S <thejaswi4uns@gmail.com>
2025-10-10 23:10:29 +0530

    Roman to Integer Problem Solved

---
+++ src/RomanToInteger/Main.java
@@ -0,0 +1,12 @@
+package RomanToInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.romanToInt("III"));
+        System.out.println(sol.romanToInt("IV"));
+        System.out.println(sol.romanToInt("IX"));
+        System.out.println(sol.romanToInt("LVIII"));
+        System.out.println(sol.romanToInt("MCMXCIV"));
+    }
+}
