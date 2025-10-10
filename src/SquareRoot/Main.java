3e9d250539764e2a0cb49041ea0f32c514f54871
Thejaswi S <thejaswi4uns@gmail.com>
2025-10-10 23:20:30 +0530

    Sqrt(x) Problem Solved

---
+++ src/SquareRoot/Main.java
@@ -0,0 +1,13 @@
+package SquareRoot;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int x = 16;
+
+        int sqrt = solution.mySqrt(x);
+
+        System.out.println("The integer square root of " + x + " is: " + sqrt);
+    }
+}
