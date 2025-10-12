66afff5462b14ef202ee6b833702124262fb6127
Thejaswi S <thejaswi4uns@gmail.com>
2025-10-13 02:25:06 +0530

    Fibonacci Number Problem Solved using Memoization

---
+++ src/FibonacciNumber/Main.java
@@ -0,0 +1,12 @@
+package FibonacciNumber;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int n = 10;
+        int result = solution.fib(n);
+
+        System.out.println(result);
+    }
+}
---
+++ src/FibonacciNumber/Solution.java
@@ -0,0 +1,16 @@
+package FibonacciNumber;
+
+import java.util.HashMap;
+
+public class Solution {
+    private final HashMap<Integer, Integer> memo = new HashMap<>();
+
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (memo.containsKey(n)) return memo.get(n);
+        int result = fib(n - 1) + fib(n - 2);
+        memo.put(n, result);
+        return result;
+    }
+}
