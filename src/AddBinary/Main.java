cc67e577ee4593af50bd57eeef4b3aadc0094220
Thejaswi S <thejaswi4uns@gmail.com>
2025-10-15 00:39:58 +0530

    Add Binary Problem Solved

---
+++ src/AddBinary/Main.java
@@ -0,0 +1,11 @@
+package AddBinary;
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "11";
+        String b = "1";
+
+        String result = Solution.addBinary(a, b);
+        System.out.println("Sum: " + result);
+    }
+}
