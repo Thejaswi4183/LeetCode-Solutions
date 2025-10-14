5a55a9d0c5a64c67a775c903f7a7db5f6ae9f6e5
Thejaswi S <thejaswi4uns@gmail.com>
2025-10-14 05:32:37 +0530

    Longest Common Prefix : Problem Solved

---
+++ src/LongestCommonPrefix/Main.java
@@ -0,0 +1,9 @@
+package LongestCommonPrefix;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        Solution solution = new Solution();
+        System.out.println(solution.longestCommonPrefix(strs));
+    }
+}
