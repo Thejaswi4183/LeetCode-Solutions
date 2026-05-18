c0fdae25d1a0f9466348fd8fde7fce37e2858bec
Thejaswi S <thejaswi4uns@gmail.com>
2026-05-18 21:12:43 +0530

    Happy Number Problem Solved

---
+++ src/HappyNumber/Main.java
@@ -0,0 +1,15 @@
+package HappyNumber;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        int n = 19;
+
+        boolean result = sol.isHappy(n);
+
+        System.out.println(result);
+    }
+}
