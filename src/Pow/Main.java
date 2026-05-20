f83253a4225b93c7a3aa95eb909fff5c5970beb5
Thejaswi S <thejaswi4uns@gmail.com>
2026-05-20 09:07:34 +0530

    Pow(x,n) Problem Solved

---
+++ src/Pow/Main.java
@@ -0,0 +1,11 @@
+package Pow;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        double x = 2.0;
+        int n = 10;
+        double result = sol.myPow(x, n);
+        System.out.println("Result = " + result);
+    }
+}
