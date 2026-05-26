fcfd9e39449e89a9408e32eae21ef7cac493463c
Thejaswi S <thejaswi4uns@gmail.com>
2026-05-26 12:23:17 +0530

    Factorial Trailing Zeroes Problem Solved

---
+++ src/FactorialTrailingZeroes/Main.java
@@ -0,0 +1,9 @@
+package FactorialTrailingZeroes;
+
+public class Main{
+    public static void main(String[] args){
+        Solution sol=new Solution();
+        int n=3;
+        System.out.println(sol.trailingZeroes(n));
+    }
+}
