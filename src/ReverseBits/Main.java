3e572c3b65e6eb77c67b5d85f1373c0bf089f97f
Thejaswi S <thejaswi4uns@gmail.com>
2026-05-27 17:16:26 +0530

    Reverse Bits Problem Solved

---
+++ src/ReverseBits/Main.java
@@ -0,0 +1,10 @@
+package ReverseBits;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 43261596;
+        int result = sol.reverseBits(n);
+        System.out.println(result);
+    }
+}
