f756df3aaca462aa92edc4e8af3e579c2463b290
Thejaswi S <thejaswi4uns@gmail.com>
2026-05-28 12:02:39 +0530

    Group Anagrams Problem Solved

---
+++ src/GroupAnagrams/Main.java
@@ -0,0 +1,11 @@
+package GroupAnagrams;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = solution.groupAnagrams(strs);
+        System.out.println(result);
+    }
+}
