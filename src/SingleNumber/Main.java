3536b1d3212cefe19c5390aeec1d02e653811d0a
Thejaswi S <thejaswi4uns@gmail.com>
2026-05-30 05:48:46 +0530

    Single Number Problem Solved

---
+++ src/SingleNumber/Main.java
@@ -0,0 +1,9 @@
+package SingleNumber;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println(solution.singleNumber(nums));
+    }
+}
