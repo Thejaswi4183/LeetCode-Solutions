c1cedf54fc5ef8d3aeb25582ce8feec82b98f791
Thejaswi S <thejaswi4uns@gmail.com>
2026-06-05 06:27:47 +0530

    Jump Game II Problem Solved

---
+++ src/JumpGameII/Main.java
@@ -0,0 +1,9 @@
+package JumpGameII;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums1 = {2, 3, 1, 1, 4};
+        System.out.println(sol.jump(nums1));
+    }
+}
