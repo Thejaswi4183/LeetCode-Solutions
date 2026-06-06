1ec35256b9aa6522b5a04b5d7bea29fbd5fc1b42
Thejaswi S <thejaswi4uns@gmail.com>
2026-06-06 09:30:15 +0530

    Bitwise AND of Numbers Range Problem Solved

---
+++ src/BitwiseANDofNumbersRange/Main.java
@@ -0,0 +1,10 @@
+package BitwiseANDofNumbersRange;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int left = 5;
+        int right = 7;
+        System.out.println(solution.rangeBitwiseAnd(left, right));
+    }
+}
