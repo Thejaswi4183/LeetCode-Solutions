840e711aa7afb6611c29ab417ce0dd8f517b6456
Thejaswi S <thejaswi4uns@gmail.com>
2026-06-09 11:05:52 +0530

    Three Sum Problem Solved

---
+++ src/ThreeSum/Main.java
@@ -0,0 +1,12 @@
+package ThreeSum;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = solution.threeSum(nums);
+        System.out.println(result);
+    }
+}
