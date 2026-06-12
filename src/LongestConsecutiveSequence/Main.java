abce2839901c8b5f0cbac75ae0956f305a4457bf
Thejaswi S <thejaswi4uns@gmail.com>
2026-06-12 10:02:01 +0530

    Longest Consecutive Sequence Problem Solved

---
+++ src/LongestConsecutiveSequence/Main.java
@@ -0,0 +1,10 @@
+package LongestConsecutiveSequence;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int result = solution.longestConsecutive(nums);
+        System.out.println(result);
+    }
+}
