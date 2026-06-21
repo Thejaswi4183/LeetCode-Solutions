7fb095fe5beddaa6175318699cd501ef09277cb1
Thejaswi S <thejaswi4uns@gmail.com>
2026-06-21 05:33:02 +0530

    Spiral Matrix Problem Solved

---
+++ src/SpiralMatrix/Main.java
@@ -0,0 +1,14 @@
+package SpiralMatrix;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        Solution sol = new Solution();
+        System.out.println(sol.spiralOrder(matrix));
+    }
+}
