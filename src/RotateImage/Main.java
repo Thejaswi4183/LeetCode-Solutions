1fc91ae356a6669b5e12f9875989f8f2e83f48f3
Thejaswi S <thejaswi4uns@gmail.com>
2026-06-25 05:36:58 +0530

    Rotate Image Problem Solved

---
+++ src/RotateImage/Main.java
@@ -0,0 +1,20 @@
+package RotateImage;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        Solution sol = new Solution();
+        sol.rotate(matrix);
+
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
