d4aad7a3e9d300671728c27de1d3db4ec89732c6
Thejaswi S <thejaswi4uns@gmail.com>
2026-07-09 10:32:03 +0530

    Remove Duplicates from Sorted List II Problem Solved

---
+++ src/RemoveDuplicatesFromSortedListII/ListNode.java
@@ -0,0 +1,10 @@
+package RemoveDuplicatesFromSortedListII;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/RemoveDuplicatesFromSortedListII/Main.java
@@ -0,0 +1,36 @@
+package RemoveDuplicatesFromSortedListII;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Creating list: 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        Solution solution = new Solution();
+        ListNode result = solution.deleteDuplicates(head);
+
+        System.out.println("After Removing Duplicates:");
+        printList(result);
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
