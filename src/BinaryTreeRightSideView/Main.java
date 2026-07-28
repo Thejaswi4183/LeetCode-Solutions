991d20b67369af198a455723fd7f3aec995f0099
Thejaswi S <thejaswi4uns@gmail.com>
2026-07-28 05:55:31 +0530

    Binary Tree Right Side View Problem Solved

---
+++ src/BinaryTreeRightSideView/Main.java
@@ -0,0 +1,19 @@
+package BinaryTreeRightSideView;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(4);
+
+        Solution solution = new Solution();
+        List<Integer> result = solution.rightSideView(root);
+
+        System.out.println(result);
+    }
+}
---
+++ src/BinaryTreeRightSideView/TreeNode.java
@@ -0,0 +1,12 @@
+package BinaryTreeRightSideView;
+
+public class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
