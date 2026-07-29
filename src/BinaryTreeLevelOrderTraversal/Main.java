33f7b8bef71fd21be4f8c691c9066cc692b3045d
Thejaswi S <thejaswi4uns@gmail.com>
2026-07-29 05:37:29 +0530

    Binary Tree Level Order Traversal Problem Solved

---
+++ src/BinaryTreeLevelOrderTraversal/Main.java
@@ -0,0 +1,18 @@
+package BinaryTreeLevelOrderTraversal;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.levelOrder(root);
+
+        System.out.println(result);
+    }
+}
