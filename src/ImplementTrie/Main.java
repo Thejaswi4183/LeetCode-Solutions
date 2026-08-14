3f1dc6ebdff31ba41e439b14e9a6fcd485fcc74e
Thejaswi S <thejaswi4uns@gmail.com>
2026-08-14 11:11:33 +0530

    Implement Trie (Prefix Tree) Problem Solved

---
+++ src/ImplementTrie/Main.java
@@ -0,0 +1,18 @@
+package ImplementTrie;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Trie trie = new Trie();
+
+        trie.insert("apple");
+
+        System.out.println(trie.search("apple"));
+        System.out.println(trie.search("app"));
+        System.out.println(trie.startsWith("app"));
+
+        trie.insert("app");
+
+        System.out.println(trie.search("app"));
+    }
+}
---
+++ src/ImplementTrie/Trie.java
@@ -0,0 +1,63 @@
+package ImplementTrie;
+
+class Trie {
+
+    private static class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd;
+    }
+
+    private final TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (curr.children[index] == null) {
+                curr.children[index] = new TrieNode();
+            }
+
+            curr = curr.children[index];
+        }
+
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (curr.children[index] == null) {
+                return false;
+            }
+
+            curr = curr.children[index];
+        }
+
+        return curr.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+
+            if (curr.children[index] == null) {
+                return false;
+            }
+
+            curr = curr.children[index];
+        }
+
+        return true;
+    }
+}
