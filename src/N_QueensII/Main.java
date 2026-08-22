4db98b80a08c92095d3eb722e56b34ee5edbcfd9
Thejaswi S <thejaswi4uns@gmail.com>
2026-08-22 10:24:09 +0530

    N-Queens II Problem Solved

---
+++ src/N_QueensII/Main.java
@@ -0,0 +1,13 @@
+package N_QueensII;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        int n = 4;
+
+        int result = solution.totalNQueens(n);
+        System.out.println(result);
+    }
+}
---
+++ src/N_QueensII/Solution.java
@@ -0,0 +1,56 @@
+package N_QueensII;
+
+import java.util.Arrays;
+
+class Solution {
+
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        return backtrack(0, board);
+    }
+
+    private int backtrack(int row, char[][] board) {
+
+        if (row == board.length) {
+            return 1;
+        }
+
+        int count = 0;
+
+        for (int col = 0; col < board.length; col++) {
+
+            if (isSafe(row, col, board)) {
+                board[row][col] = 'Q';
+                count += backtrack(row + 1, board);
+                board[row][col] = '.';
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isSafe(int row, int col, char[][] board) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
