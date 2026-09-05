4fb407c3e54a9832894dd51ef66ed59bf93f7c8e
Thejaswi S <thejaswi4uns@gmail.com>
2026-09-05 10:06:46 +0530

    House Robber Problem Solved

---
+++ src/HouseRobber/Main.java
@@ -0,0 +1,10 @@
+package HouseRobber;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 9, 3, 1};
+        Solution solution = new Solution();
+        System.out.println(solution.rob(nums));
+    }
+}
+
