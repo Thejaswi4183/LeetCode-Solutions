a5ec360d6b9115436068ad131e5e48d7938b4646
Thejaswi S <thejaswi4uns@gmail.com>
2026-09-19 12:03:57 +0530

    Merge Strings Alternately Problem Solved

---
+++ src/MergeStringsAlternately/Main.java
@@ -0,0 +1,11 @@
+package MergeStringsAlternately;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String word1 = "abc";
+        String word2 = "pqr";
+        String result = solution.mergeAlternately(word1, word2);
+        System.out.println(result);
+    }
+}
