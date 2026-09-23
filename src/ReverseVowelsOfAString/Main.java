a3688983ef171e2b6aaf39e8b5fdae606a475674
Thejaswi S <thejaswi4uns@gmail.com>
2026-09-23 11:41:23 +0530

    Reverse Vowels of a String Problem Solved

---
+++ src/ReverseVowelsOfAString/Main.java
@@ -0,0 +1,9 @@
+package ReverseVowelsOfAString;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "IceCreAm";
+        System.out.println(solution.reverseVowels(s));
+    }
+}
